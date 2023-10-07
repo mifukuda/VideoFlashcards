00dcdb0b96c06dab7f0dd2ff089fa5c2535e8e97
Minato Fukuda <minato.fukuda@stonybrook.edu>
2023-10-06 22:46:46 -0400

    Flip card functionality

--- client/src/components/Flashcard.js
+++ client/src/components/Flashcard.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export default function Flashcard(props) {
+    const [flip, setFlip] = useState(false);
+    
+    function handleClick() {
+        setFlip(!flip);
+    }
     
     return (
         <div className="flashcard">
-            <div className="flashcard-header">
-                <h2>{props.title}</h2>
+            <div className={`flashcard-inner ${flip ? "flip" : ""}`}>
+                <div className="flashcard-back">
+                    <div className="flashcard-header">
+                        <h2>{props.title}</h2>
+                        </div>
+                    <div className="flashcard-description">
+                        <p>{props.description}</p>
+                    </div>
+                </div>
+                <div className="flashcard-front">
+                    <video draggable="false" playsInline loop hash="LOFimKX" autoPlay controls muted width="100%" className="flashcard-video">
+                        <source type="video/mp4" src={props.url}/>
+                    </video>
+                </div>
             </div>
-            <video draggable="false" playsInline loop hash="LOFimKX" autoPlay controls muted width="100%" className="flashcard-video">
-                <source type="video/mp4" src={props.url}/>
-            </video>
-            <div className="flashcard-description">
-                <p>{props.description}</p>
-            </div>
+            <button className="flip-button" onClick={() => handleClick()}></button>
         </div>
     )
 }
